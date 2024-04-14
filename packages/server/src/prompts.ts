f0aadb8dffd245f2e372d3a7a40d5d70b5fa5399
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-04-14 19:40:55 +0800

    feat: meme add more

--- packages/server/src/prompts.ts
+++ packages/server/src/prompts.ts
@@ -9,6 +9,19 @@ export const soloPrompts = [
   'What is your favorite animal?',
   'What is your favorite flower?',
   'Draw your family!',
+  'What is your favorite fruit?',
+  'Your best friend!',
+  'What do you wanna say to your boss?',
+  'Draw Changi City Point!',
+  'Your best memory...',
+  'What is your favorite movie?',
+  'Draw your favorite book!',
+  'What is your favorite song?',
+  'What is your favorite game?',
+  'What is your favorite sport?',
+  'What is your favorite food?',
+  'What is your favorite drink?',
+  'New year resolution?',
 ]
 
 export const sharedPrompts = [
@@ -16,4 +29,11 @@ export const sharedPrompts = [
   'The Merlion',
   'EXPO',
   'SUTD',
+  'The Jewel',
+  'The Marina Bay Sands',
+  'The Esplanade',
+  'The ArtScience Museum',
+  'The Helix Bridge',
+  'The Singapore Flyer',
+  'The Botanic Gardens',
 ]
